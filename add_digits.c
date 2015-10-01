7335b9c4bec99f58bdc76821e810b6dc84aab600
lilmuggle <516674277@qq.com>
2015-10-01 16:44:04 +0800

    again

--- add_digits.c
+++ add_digits.c
@@ -1,12 +1,12 @@
+//10^n กิ 1 (mod 9)
 int addDigits(int num)
 {
-    int s, y;
-    s = num / 10;
-    while(s != 0)
-    {
-        y = num % 10;
-        num = y + s;
-        s = num / 10;
-    }
-    return num;
+    int res;
+
+    if(num == 0)
+        return 0;
+    res = num % 9;
+    if(res == 0)
+        return 9;
+    return res;
 }
--- isPowerOfTwo.c
+++ isPowerOfTwo.c
@@ -1,8 +1,6 @@
 bool isPowerOfTwo(int n)
 {
-    while(n % 2 == 0 && n != 0)
-        n /= 2;
-    if(n == 1)
-        reuturn true;
-    return false;
+    if(n <= 0)
+        return false;
+    return (n & (n - 1) == 0);
 }
