fb8851b05c403bb9442a7602f5918796623babba
lilmuggle <516674277@qq.com>
2015-10-17 21:29:27 +0800

    #104

---
+++ maxDepth.c
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode* root) {
+    if(root == NULL)
+        return 0;
+    int l = maxDepth(root->left);
+    int r = maxDepth(root->right);
+    return l>r:l+1:r+1;
+}
