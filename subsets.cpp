a134c75ad0620fa13c0736010db7c16d4f905e06
lilmuggle <516674277@qq.com>
2015-11-01 14:00:55 +0800

    #78

--- subsets.cpp
+++ subsets.cpp
@@ -23,3 +23,39 @@ public:
         return vec;
     }
 };
+
+//Another backtracking
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> subs;
+        vector<int> sub;
+        genSubsets(nums, 0, sub, subs);
+        return subs;
+    }
+    void genSubsets(vector<int>& nums, int start, vector<int>& sub, vector<vector<int>>& subs) {
+        subs.push_back(sub);
+        for (int i = start; i < nums.size(); i++) {
+            sub.push_back(nums[i]);
+            genSubsets(nums, i + 1, sub, subs);
+            sub.pop_back();
+        }
+    }
+};
+
+//Bit Manipulation
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        sort (S.begin(), S.end());
+        int elem_num = S.size();
+        int subset_num = pow (2, elem_num);
+        vector<vector<int> > subset_set (subset_num, vector<int>());
+        for (int i = 0; i < elem_num; i++)
+            for (int j = 0; j < subset_num; j++)
+                if ((j >> i) & 1)
+                    subset_set[j].push_back (S[i]);
+        return subset_set;
+    }
+};
