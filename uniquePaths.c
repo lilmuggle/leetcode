00a05cc802e6a5806903ca26980763a7ff658d31
lilmuggle <516674277@qq.com>
2015-11-02 13:31:03 +0800

    #62

--- uniquePaths.c
+++ uniquePaths.c
@@ -8,3 +8,30 @@ int uniquePaths(int m, int n) {
     }
     return (int)prod;
 }
+
+//DP
+
+class Solution {
+    int uniquePaths(int m, int n) {
+        vector<vector<int> > path(m, vector<int> (n, 1));
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                path[i][j] = path[i - 1][j] + path[i][j - 1];
+        return path[m - 1][n - 1];
+    }
+};
+
+//Optimized
+class Solution {
+    int uniquePaths(int m, int n) {
+        if (m > n) return uniquePaths(n, m);
+        vector<int> pre(m, 1);
+        vector<int> cur(m, 1);
+        for (int j = 1; j < n; j++) {
+            for (int i = 1; i < m; i++)
+                cur[i] = cur[i - 1] + pre[i];
+            swap(pre, cur);
+        }
+        return pre[m - 1];
+    }
+};
