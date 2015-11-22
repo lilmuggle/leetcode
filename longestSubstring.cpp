4b4bda03f041c090ce4ff60d16bd99c9d8d1b192
lilmuggle <516674277@qq.com>
2015-11-22 15:37:12 +0800

    #3

--- longestSubstring.cpp
+++ longestSubstring.cpp
@@ -21,3 +21,20 @@ public:
         return helper(s, 0);
     }
 };
+
+//O(n)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxlen = 0, start = 0;
+        vector<int> a(95, -1);
+        for(int i = 0; i < s.size(); i++) {
+            start = max(a[s[i] - 32] + 1, start);
+            a[s[i] - 32] = i;
+            maxlen = max(maxlen, i + 1 - start);
+            if(maxlen == 95)
+                break;
+        }
+        return maxlen;
+    }
+};
