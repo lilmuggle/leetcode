b5763492d6fa800f80eb024ecc441ecafcb8c800
lilmuggle <516674277@qq.com>
2015-12-08 14:38:48 +0800

    #85

--- maximalRectangle.cpp
+++ maximalRectangle.cpp
@@ -26,7 +26,7 @@ public:
 };
 
 //Another
-public class Solution {
+class Solution {
     public int maximalRectangle(char[][] matrix) {
         if (matrix==null||matrix.length==0||matrix[0].length==0)
             return 0;
