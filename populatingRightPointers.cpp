adc9c946291e7eb2f795733508f4ee40bc0c0646
lilmuggle <516674277@qq.com>
2015-12-10 15:25:13 +0800

    #117

---
+++ populatingRightPointers.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if(!root)
+            return;
+        TreeLinkNode *pre = NULL, *cur;
+        queue<TreeLinkNode*> q;
+        q.push(root);
+        q.push(NULL);
+        while(!q.empty()) {
+            cur = q.front();
+            q.pop();
+            if(!cur) {
+                if(q.size() > 0)
+                    q.push(NULL);
+            }
+            else {
+                if(cur->left)
+                    q.push(cur->left);
+                if(cur->right)
+                    q.push(cur->right);
+            }
+            if(pre)
+                pre->next = cur;
+            pre = cur;
+        }
+    }
+};
