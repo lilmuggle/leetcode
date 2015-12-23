3e1d1a0199b88ec8d94419963933f0144c97eea2
lilmuggle <516674277@qq.com>
2015-12-23 15:06:00 +0800

    #316

---
+++ removeDupLet.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        if(s.empty())
+            return "";
+        int letters[26] = {0};
+        int pos = 0;
+        for(char c : s)
+            letters[c - 'a']++;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] < s[pos])
+                pos = i;
+            if(--letters[s[i] - 'a'] == 0)
+                break;
+        }
+        string ss = s.substr(pos + 1);
+        int len = ss.size();
+        for(int i = 0; i < ss.size(); i++)
+            if(ss[i] == s[pos])
+                ss.erase(ss.begin() + i--);
+        return s[pos] + removeDuplicateLetters(ss);
+    }
+};
+
+//Another
+string removeDuplicateLetters(string s) {
+    vector<unsigned int> cnt(26,0); //only consider lowercase letters
+    vector<bool> inRes(26, false); //true if the letter has been added to res
+    for(char ch:s) cnt[ ch-'a' ]++;
+    string res = ""; //use res as a stack
+    for(char ch:s){
+       cnt[ ch-'a' ]--;
+       if(res.empty()){
+           res.push_back(ch);
+           inRes[ ch-'a' ] = true;
+           continue;
+       }
+       if(inRes[ch-'a']) continue;
+       while(ch<res.back() && !res.empty() && cnt[ res.back()-'a' ]>0){
+           inRes[ res.back()-'a' ] = false;
+           res.pop_back();
+
+       }
+       res.push_back(ch);
+       inRes[ ch-'a' ] = true;
+    }
+    return res;
+}
